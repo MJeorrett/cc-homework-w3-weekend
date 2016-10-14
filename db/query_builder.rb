2bb6362cd39c476cf56163cc0a95ea08f5899005
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-14 13:21:12 +0100

    refactoring

--- db/query_builder.rb
+++ db/query_builder.rb
@@ -8,19 +8,29 @@ class QueryBuilder
     conditions_array = []
 
     for column, value in conditions_hash
-      if value.class == Fixnum
-        str = "#{column} = #{value}"
-      else
-        str = "#{column} = '#{value}'"
-      end
-
-      conditions_array.push(str)
+      value_sql = self.value_to_sql(value)
+      sql = "#{column} = #{value_sql}"
+      conditions_array.push(sql)
     end
 
-    condition_statment = conditions_array.join(" AND ")
+    condition_statement = conditions_array.join(" AND ")
     select_statement = self.all_records(table_name)
 
     return "#{select_statement} WHERE #{condition_statement}"
+  end
+
+  def self.value_to_sql( value )
+    value_class = value.class().to_s()
+    case value_class
+    when 'Fixnum'
+      sql = value
+    when 'String'
+      sql = "'#{value}'"
+    else
+      raise(TypeError, "Error: Un-supported data type #{value_class}.")
+    end
+
+    return sql
   end
 
 end
