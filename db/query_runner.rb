dcbc18417748f7856faa990b8b90df970545bb3e
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-14 15:18:05 +0100

    changing db name to class constant

--- db/query_runner.rb
+++ db/query_runner.rb
@@ -2,9 +2,11 @@ require('pg')
 
 class QueryRunner
 
+  DB_NAME = 'cinema'
+
   def self.run( sql )
     begin
-      db = PG.connect(dbname: 'cinema', host: 'localhost')
+      db = PG.connect(dbname: DB_NAME, host: 'localhost')
       result = db.exec( sql )
     ensure
       db.close()
