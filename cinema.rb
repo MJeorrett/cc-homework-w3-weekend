6828b240f65703249a148e124f8bd8e7e5062fc5
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-15 22:43:40 +0100

    adding create ticket

--- cinema.rb
+++ cinema.rb
@@ -1,11 +1,19 @@
+require_relative('models/tickets')
+
 class Cinema
 
- def sell_ticket( customer, film )
+ def self.sell_ticket( customer, film )
    if customer.funds < film.price
      raise "Error: customer can't afford ticket!"
    else
+     ticket_data = {
+       customer_id: customer.id,
+       film_id: film.id
+     }
+     ticket = Ticket.new( ticket_data )
+     ticket.save
      customer.funds -= film.price
-     customer.save
+     customer.update
    end
  end
 
