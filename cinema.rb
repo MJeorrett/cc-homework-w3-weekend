80242662002eef5cfd1cc7b421e3d9dff61fc7c3
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-16 14:27:59 +0100

    correcting indentation

--- cinema.rb
+++ cinema.rb
@@ -2,19 +2,19 @@ require_relative('models/ticket')
 
 class Cinema
 
- def self.sell_ticket( customer, film )
-   if customer.funds < film.price
-     raise "Error: customer can't afford ticket!"
-   else
-     ticket_data = {
-       customer_id: customer.id,
-       film_id: film.id
-     }
-     ticket = Ticket.new( ticket_data )
-     ticket.save
-     customer.funds -= film.price
-     customer.update
-   end
- end
+def self.sell_ticket( customer, film )
+  if customer.funds < film.price
+    raise "Error: customer can't afford ticket!"
+  else
+    ticket_data = {
+      customer_id: customer.id,
+      film_id: film.id
+    }
+    ticket = Ticket.new( ticket_data )
+    ticket.save
+    customer.funds -= film.price
+    customer.update
+  end
+end
 
 end
