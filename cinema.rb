1340baee4f08be134b5505faaa3bc38f83380a38
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-17 13:19:20 +0100

    adding sell_snack method to Cinema class

--- cinema.rb
+++ cinema.rb
@@ -2,24 +2,39 @@ require_relative('models/ticket')
 
 class Cinema
 
-def self.sell_ticket( customer, film )
+  def self.sell_ticket( customer, film )
 
-  if customer.funds < film.price
+    if customer.funds < film.price
+      raise "Error: customer can't afford ticket!"
+    else
+      ticket_data = {
+        'customer_id' => customer.id,
+        'film_id' => film.id
+      }
 
-    raise "Error: customer can't afford ticket!"
-  else
+      ticket = Ticket.new( ticket_data )
+      ticket.save()
 
-    ticket_data = {
-      'customer_id' => customer.id,
-      'film_id' => film.id
-    }
-
-    ticket = Ticket.new( ticket_data )
-    ticket.save
-
-    customer.funds -= film.price
-    customer.update
+      customer.funds -= film.price
+      customer.update()
+    end
   end
-end
+
+  def self.sell_snack( customer, snack )
+    if customer.funds < snack.price
+      raise "Error: customer can't afford snack!"
+    else
+      customer_snack_data = {
+        'customer_id' => customer.id
+        'snack_id' => snack.id
+      }
+
+      customer_snack = CustomerSnack.new( customer_snack_data )
+      customer_snack.save()
+
+      customer.funds -= snack.price
+      customer.update()
+    end
+  end
 
 end
